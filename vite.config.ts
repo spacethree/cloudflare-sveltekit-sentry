26cb5997c764fc64670a49d207a62cadf80da436
Jame Mackson <jamemackson@icloud.com>
2025-06-23 09:45:54 -0500

    work around sentry bug
    https://github.com/sveltejs/kit/issues/13869#issuecomment-2958203909

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { sentrySvelteKit } from "@sentry/sveltekit";
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sentrySvelteKit({
-        sourceMapsUploadOptions: {
-            org: "space3",
-            project: "cloudflare-sveltekit"
-        }
-    }), sveltekit()]
+  plugins: [
+    sentrySvelteKit({
+      sourceMapsUploadOptions: {
+        org: "space3",
+        project: "cloudflare-sveltekit",
+      },
+    }),
+    sveltekit(),
+  ],
+  ssr: {
+    external: ["@sentry/sveltekit"],
+  },
 });
