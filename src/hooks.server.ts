cc36990acc3a3fc60b77a52b7fee6754e3158339
Jame Mackson <jamemackson@icloud.com>
2025-06-23 09:58:24 -0500

    debug: log out Sentry object

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,11 @@
 import { sequence } from "@sveltejs/kit/hooks";
 import { handleErrorWithSentry, sentryHandle } from "@sentry/sveltekit";
-import * as Sentry from '@sentry/sveltekit';
+import * as Sentry from "@sentry/sveltekit";
+
+console.log("Sentry =>", Sentry);
 
 Sentry.init({
-  dsn: 'https://62c179208e23abfaca674357ecd68744@o4508894715641856.ingest.us.sentry.io/4509548440649728',
+  dsn: "https://62c179208e23abfaca674357ecd68744@o4508894715641856.ingest.us.sentry.io/4509548440649728",
 
   tracesSampleRate: 1.0,
 
