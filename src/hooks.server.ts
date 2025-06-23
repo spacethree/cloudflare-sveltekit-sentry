c84889c9460560ab0c951c68aab14ae7cc3e121d
Jame Mackson <jamemackson@icloud.com>
2025-06-23 15:57:46 -0500

    disable Sentry.init (missed in last step)

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -8,14 +8,14 @@ import * as Sentry from "@sentry/sveltekit";
 
 console.log("Sentry =>", Sentry);
 
-Sentry.init({
-  dsn: "https://62c179208e23abfaca674357ecd68744@o4508894715641856.ingest.us.sentry.io/4509548440649728",
+// Sentry.init({
+//   dsn: "https://62c179208e23abfaca674357ecd68744@o4508894715641856.ingest.us.sentry.io/4509548440649728",
 
-  tracesSampleRate: 1.0,
+//   tracesSampleRate: 1.0,
 
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: import.meta.env.DEV,
-});
+//   // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+//   // spotlight: import.meta.env.DEV,
+// });
 
 export const handle = sequence(
   initCloudflareSentryHandle({
